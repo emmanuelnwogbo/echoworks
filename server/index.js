3ba8bdd321af7212162773a02f30c20664219e53
emmanuelnwogbo <nerdyemmanuel@gmail.com>
2023-01-24 02:08:08 +0100

    init

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+import "regenerator-runtime/runtime.js";
+import express from "express";
+import http from "http";
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({
+  extended: false
+}));
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 8080;
+const server = http.createServer(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+server.listen(PORT, async (error) => {
+    if (error) {
+      return error;
+    }
+  
+    return console.log(`server started on port here ${PORT}`);
+});
